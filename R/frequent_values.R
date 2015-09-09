7b5ac815364cad729d55df51ae967758fab4649b
paulhendricks <paul.hendricks.2013@owu.edu>
2015-09-09 16:45:04 +0000

    formatting

--- R/frequent_values.R
+++ R/frequent_values.R
@@ -9,18 +9,29 @@
 #' @export
 frequent_values <- function(.x, n = 5L, type = "most") {
   if(!is.atomic(.x)) stop(".x must be an atomic vector.")
-  if(length(n) != 1L || !is.numeric(n) || any(n < 1)) stop("n must be a single positive integer.")
-  if(!(type %in% c("most", "least"))) stop("type takes only two values: 'most' or 'least'")
-  values <- sort(tapply(as.character(.x), factor(.x), length), decreasing = TRUE)
+  if(length(n) != 1L || !is.numeric(n) || any(n < 1)) {
+    stop("n must be a single positive integer.")
+  }
+  if(!(type %in% c("most", "least"))) {
+    stop("type takes only two values: 'most' or 'least'")
+  }
+  values <- sort(tapply(as.character(.x), factor(.x), length),
+                 decreasing = TRUE)
+
   if (type == "most"){
     values_processed <-
-      concat_rank_value_occurrence(r = unname(utils::head(rank(-values, ties.method = "min", na.last = FALSE), n)),
+      concat_rank_value_occurrence(r = unname(utils::head(rank(-values,
+                                                               ties.method = "min",
+                                                               na.last = FALSE), n)),
                                    n = utils::head(names(values), n),
                                    o = utils::head(unname(values), n))
   } else {
-    values_processed <- concat_rank_value_occurrence(r = unname(rev(utils::tail(rank(-values, ties.method = "min", na.last = FALSE), n))),
-                                                     n = rev(utils::tail(names(values), n)),
-                                                     o = rev(utils::tail(unname(values), n)))
+    values_processed <-
+      concat_rank_value_occurrence(r = unname(rev(utils::tail(rank(-values,
+                                                                   ties.method = "min",
+                                                                   na.last = FALSE), n))),
+                                   n = rev(utils::tail(names(values), n)),
+                                   o = rev(utils::tail(unname(values), n)))
   }
   return(paste(values_processed, collapse = " "))
 }
